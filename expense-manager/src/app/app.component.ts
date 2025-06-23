efab32e6bef421f5e4c9a0fb41506612b410a601
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-06-23 16:14:33 +0630

    update

--- expense-manager/src/app/app.component.ts
+++ expense-manager/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ExpenseEntryComponent} from './expense-entry/expense-entry.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExpenseEntryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ expense-manager/src/app/my-lifecycle-sample/my-lifecycle-sample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-lifecycle-sample',
+  imports: [],
+  templateUrl: './my-lifecycle-sample.component.html',
+  styleUrl: './my-lifecycle-sample.component.scss'
+})
+export class MyLifecycleSampleComponent {
+
+}
