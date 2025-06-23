a06864957ee291aaa3ada34f31d8a73ac293f024
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-06-23 16:41:54 +0630

    update

--- expense-manager/src/app/app.component.ts
+++ expense-manager/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ExpenseEntryComponent} from './expense-entry/expense-entry.component';
+import {MyLifecycleSampleComponent} from './my-lifecycle-sample/my-lifecycle-sample.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ExpenseEntryComponent],
+  imports: [RouterOutlet, ExpenseEntryComponent, MyLifecycleSampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- expense-manager/src/app/my-lifecycle-sample/my-lifecycle-sample.component.ts
+++ expense-manager/src/app/my-lifecycle-sample/my-lifecycle-sample.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-lifecycle-sample',
@@ -6,6 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './my-lifecycle-sample.component.html',
   styleUrl: './my-lifecycle-sample.component.scss'
 })
-export class MyLifecycleSampleComponent {
+export class MyLifecycleSampleComponent implements OnInit{
 
+  constructor() {
+  }
+  ngOnChanges(): void {
+    console.log("Change detection");
+  }
+  ngOnInit(): void {
+        console.log("Initialization of Component/Directive");
+    }
+  ngDoCheck(){
+    console.log("Custom change detection");
+  }
+  ngAfterContentInit(){
+    console.log("Content initialization");
+  }
+
+  ngAfterContentChecked(){
+    console.log("Checking changes in content");
+  }
+  ngAfterViewInit(){
+    console.log("View initialization");
+  }
+
+  ngAfterViewChecked() {
+    console.log("Checking changes in views")
+  }
+
+  ngOnDestroy() {
+    console.log("Destruction of component / directive")
+  }
 }
