95790c7a5cf35ecec779f96628ae3ff27056e3dc
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-07-02 20:03:49 +0630

    new:

---
+++ nestedApplication/src/app/admin-bio/admin-bio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-bio',
+  imports: [],
+  templateUrl: './admin-bio.component.html',
+  styleUrl: './admin-bio.component.scss'
+})
+export class AdminBioComponent {
+
+}
