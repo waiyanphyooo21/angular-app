28817eb7a87af71348be868dd2e0abdc663a3b69
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-07-02 20:11:50 +0630

    user admin

--- nestedApplication/src/app/app.component.ts
+++ nestedApplication/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
+import { AdminBioComponent } from './admin-bio/admin-bio.component';
+import { UserBioComponent } from './user-bio/user-bio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ParentComponent],
+  imports: [RouterOutlet, ParentComponent,AdminBioComponent,UserBioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'nestedApplication';
+  user = {"name": "Admin", "isAdmin": false};
+  isValid = false;
+  admin(){
+    this.user.isAdmin = true;
+    this.isValid = true;
+  }
 }
