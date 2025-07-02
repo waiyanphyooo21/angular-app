dc1f6275c6413765da0cbbc72cdb3173fb9dd695
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-07-02 19:38:26 +0630

    new

--- nestedApplication/src/app/app.component.ts
+++ nestedApplication/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
