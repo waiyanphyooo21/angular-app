6fa61c8fd3f6605d94d7dabf1408cccd101e5629
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-07-02 19:36:12 +0630

    new

---
+++ nestedApplication/src/app/parent/parent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  imports: [],
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.scss'
+})
+export class ParentComponent {
+
+}
