57a4bc28c80ebfe176e7b9edc846d5dc1f976e23
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-07-21 16:44:13 +0630

    update

--- angular_application/src/app/components/designation/designation.component.ts
+++ angular_application/src/app/components/designation/designation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IDesignation } from '../../model/interface/role';
 
 @Component({
   selector: 'app-designation',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.scss'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit{
+constructor(private http:HttpClient){
+
+}
+designationList:IDesignation[] = [];
+ngOnInit() {
+    this.getALlDesignations();
+  }
+
+getALlDesignations(){
+  this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation").subscribe((res:any) => {
+    this.designationList = res.data;
+  })
+}
 
 }
