ceef516d2e65806aaca65416e3f08cbc63c03ed2
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-08-04 16:26:04 +0630

    update

--- first-ng-app/src/app/pipes/filter-todos.pipe.ts
+++ first-ng-app/src/app/pipes/filter-todos.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Pipe({
   name: 'filterTodos'
 })
 export class FilterTodosPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(todos: Todo[], searchTerm: string[]): Todo[] {
+    if(!searchTerm){
+      return todos;
+    }
+    const text = searchTerm.toLowerCase();
+    return todos.filter(todo =>{
+      return (todo.title.toLowerCase().includes(text));
+    });
+}
 
 }
