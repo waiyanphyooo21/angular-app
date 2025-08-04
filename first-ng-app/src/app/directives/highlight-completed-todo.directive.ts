e3000da28d04f3ee4177f9999f9ddc7e466f8d74
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-08-04 16:33:04 +0630

    update

--- first-ng-app/src/app/directives/highlight-completed-todo.directive.ts
+++ first-ng-app/src/app/directives/highlight-completed-todo.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, effect, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightCompletedTodo]'
 })
 export class HighlightCompletedTodoDirective {
 
-  constructor() { }
+  isCompleted = input(false);
+  el = inject(ElementRef);
+
+  stylesEffect = effect(()=> {
+    if (this.isCompleted()) {
+      this.el.nativeElement.style.textDecoration = 'line-through';
+      this.el.nativeElement.style.backgroundColor = '#d3f9d8';
+      this.el.nativeElement.style.color = '#6c757d';
+  }else{
+    this.el.nativeElement.style.textDecoration = 'none';
+    this.el.nativeElement.style.backgroundColor = '#fff';
+    this.el.nativeElement.style.color = '#000';
+  }
+    
+})
 
 }
