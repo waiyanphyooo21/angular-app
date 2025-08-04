45c9ed05446d9617be37ac7a419f8c1e4be5d859
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-08-04 15:22:33 +0630

    update

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,27 +1,39 @@
-// import { Component, OnInit } from '@angular/core';
-// import { RouterLink } from '@angular/router';
-// import {TodoService} from '../services/todos.service';
+// import { Component, OnInit, inject, signal } from '@angular/core';
+
+// import { Todo } from '../model/todo.type';
+// import { CommonModule } from '@angular/common';
+// import { catchError } from 'rxjs';
+// import { TodosService } from '../services/todos.service';
 
 // @Component({
 //   selector: 'app-todos',
-//   imports: [],
+//   standalone: true,
+//   imports: [CommonModule],
 //   templateUrl: './todos.component.html',
 //   styleUrl: './todos.component.scss'
 // })
-// export class TodosComponent implements OnInit{
+// export class TodosComponent implements OnInit {
 //   todoService = inject(TodosService);
 //   todoItems = signal<Array<Todo>>([]);
 
 //   searchTerm = signal('');
-  
+
 //   ngOnInit(): void {
-//       console.log(this.todoService.todoItems);
-//       this.todoItems.set(this.todoService.todoItems);
+//     // const todos = this.todoService.getTodos();
+//     // this.todoItems.set(todos);
+
+//     this.todoService.getTodosFormAPI().pipe(
+//       catchError((error) => {
+//         console.log(error);
+//         throw error;
+//       })
+//     ).subscribe((todos)=>{
+//       this.todoItems.set(todos);
+//     })
 //   }
 // }
 
 import { Component, OnInit, inject, signal } from '@angular/core';
-
 import { Todo } from '../model/todo.type';
 import { CommonModule } from '@angular/common';
 import { catchError } from 'rxjs';
@@ -36,21 +48,20 @@ import { TodosService } from '../services/todos.service';
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
-  todoItems = signal<Array<Todo>>([]);
-
-  searchTerm = signal('');
+  todoItems = signal<Todo[]>([]);
+  loading = signal(true); 
 
   ngOnInit(): void {
-    // const todos = this.todoService.getTodos();
-    // this.todoItems.set(todos);
+  this.loading.set(true);
 
-    this.todoService.getTodosFormAPI().pipe(
-      catchError((error) => {
-        console.log(error);
-        throw error;
-      })
-    ).subscribe((todos)=>{
+  this.todoService.getTodosFormAPI().subscribe(todos => {
+    setTimeout(() => {
       this.todoItems.set(todos);
-    })
-  }
+      this.loading.set(false);
+    }, 500); 
+  }, error => {
+    console.error(error);
+    this.loading.set(false);
+  });
+}
 }
