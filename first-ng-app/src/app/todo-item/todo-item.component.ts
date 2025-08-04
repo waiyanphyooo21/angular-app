233aaad2ebb0cefdf94337fea2b2f496ef0e9dbb
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-08-04 16:45:55 +0630

    update

--- first-ng-app/src/app/todo-item/todo-item.component.ts
+++ first-ng-app/src/app/todo-item/todo-item.component.ts
@@ -1,9 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HighlightCompletedTodoDirective } from '../directives/highlight-completed-todo.directive';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [HighlightCompletedTodoDirective, UpperCasePipe],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
